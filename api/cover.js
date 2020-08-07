d4e6238c8a558ae8013ab2cc5de8c6741264aaf3
Oscar <oscar.ramos@ucsp.edu.pe>
2020-08-07 16:21:55 -0500

    Allow CORS

--- api/cover.js
+++ api/cover.js
@@ -1,7 +1,24 @@
 const axios = require('axios').default;
 const apiUrl = "http://54.193.19.36:4000/"
 
-export default (req, res) => {
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another option
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const cover = (req, res) => {
   return axios.post(apiUrl, req.body)
     .then(response => {
       res.json(response.data)
@@ -10,3 +27,5 @@ export default (req, res) => {
       res.status(400).json({ error: `error catched on proxy: '${error}'` })
     });
 }
+
+export default allowCors(cover)
